377d78c3cf79d7e297229a07b9a15b13315e080a
Marc Chehab <marcchehab@protonmail.com>
2023-10-11 16:27:44 +0200

    dictionary + colors + examples

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import argparse, os
+import file_manager as fm
 
-# Configuration variables
+# Helper variables
 testfile_empty = "testfile_empty.txt"
 testfile_small = "testfile_small.txt"
 testfile_large = "testfile_large.txt"
@@ -8,14 +9,15 @@ testfiles = [testfile_empty, testfile_small, testfile_large]
 testcontent_small = 'This is testcontent with\na new line!'
 testcontent_large = 'This is testcontent with\na new line!' * 1000
 testprefix = "test_"
-
-
+results = {"pass": 0, "fail": 0, "error": 0}
+str_pass = "\033[92m" + "pass" + "\033[0m"
+str_fail = "\033[93m" + "fail" + "\033[0m"
+str_error = "\033[91m" + "error" + "\033[0m"
 
 # Handling arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--select", help="Select the tests to run with a keyword")
 args = parser.parse_args()
-
 
 # Our helper functions
 def setup(fnc_name):
@@ -35,25 +37,47 @@ def teardown(fnc_name):
         except Exception:
             print(f"Teardown of {testfile} in {fnc_name} failed!")
 
+# Actual test functions
 
+def test_read_file_content_small_correct():
+    actual = fm.read_file(testfile_small)
+    expected = testcontent_small
+    assert actual == expected
 
-def test_printing():
-    with open(testfile_small, "r") as f:
-        print(f.read())
+def test_read_file_content_large_correct():
+    actual = fm.read_file(testfile_large)
+    expected = testcontent_large
+    assert actual == expected
 
-def test_flexibility():
-    print("very flexible")
+def test_cause_fail():
+    assert 1 == 2
+
+def test_cause_error():
+    assert 1/0 == 2
 
 
 def main():
+    print() # newline
     for name, fnc in globals().items():
-        if name.startswith(testprefix):
+        if name.startswith(testprefix) and callable(fnc):
             if not args.select or args.select in name:
                 setup(name)
-                fnc()
+                print(f"{name:<60}", end="")
+                try:
+                    fnc()
+                    print(str_pass)
+                    results["pass"] += 1
+                except AssertionError:
+                    print(str_fail)
+                    results["fail"] += 1
+                except Exception:
+                    print(str_error)
+                    results["error"] += 1
                 teardown(name)
+    print("\nTest summary:\n-------------")
+    print(f"{str_pass} {results['pass']}")
+    print(f"{str_fail} {results['fail']}")
+    print(f"{str_error} {results['error']}")
 
 if __name__ == "__main__":
     main()
-
-# test
